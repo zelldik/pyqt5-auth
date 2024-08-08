e7e8af0f0e64a302035bf68389a6897703d1a24f
zelldik <kornevsemen221@gmail.com>
2024-08-08 22:51:22 +0300

    commit

--- windows/main.py
+++ windows/main.py
@@ -4,10 +4,60 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from check_db import *
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui.setupUi(self)
 
-    def setu(self):
-        uic.loadUi('assets/main/MainWindow.ui', self)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.reg)
+        self.ui.pushButton_2.clicked.connect(self.auth)
+        self.base_line_edit = [self.ui.lineEdit, self.ui.lineEdit_2]
+        self.check_db = CheckThread()
+        self.check_db.mysignal.connect(self.signal_handler)
+
+
+    #Проверка провильности ввода
+    def check_input(funct):
+        def wrapper(self):
+            for line_edit in self.base_line_edit:
+                if len(line_edit.text()) == 0:
+                    return
+            funct(self)
+        return wrapper
+
+
+
+    #обработчик сигналов
+    def signal_handler(self, value):
+        QtWidgets.QMessageBox.about(self, 'Оповещение' value)
+
+
+
+    @check_input
+    def auth(self):
+        name = self.ui.lineEdit.text()
+        passw = self.ui.lineEdit_2.text()
+        self.check_db.thr_login(name, passw)
+
+    @check_input
+    def reg(self):
+        name = self.ui.lineEdit.text()
+        passw = self.ui.lineEdit_2.text()
+        self.check_db.thr_register(name, passw)
+
+
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mywin = MainWindow()
+    mywin.show()
+    sys.exit(app.exec_())
+
+
+    #def setu(self):
+       # uic.loadUi('assets/main/MainWindow.ui', self)
